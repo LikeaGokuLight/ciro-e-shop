81bd0372d0bc5472a3c04f706e54b64fcc9ba54b
LikeaGokuLight <vitalik1@hotmail.it>
2025-05-30 03:02:29 +0200

    Updated netlify.toml to wrap with withNetlify

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,8 @@
-import { withNetlify } from '@netlify/next'
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	reactStrictMode: true,
-	// You can add other config options here
-}
+  /* config options here */
 
-export default withNetlify(nextConfig)
+};
+
+export default nextConfig;
